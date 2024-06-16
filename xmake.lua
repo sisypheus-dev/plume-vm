8d266feb8ea3a3c7bd5cfff22e7841d130a2b33c
Sisypheus <contact@sisypheus.dev>
2024-06-16 15:37:30 +0200

    Revert "build(xmake): added BDWGC dependency (garbage collector)"
    This reverts commit 8bf907f37132d8f1a851e88774d98102a6c286b7.

--- xmake.lua
+++ xmake.lua
@@ -4,13 +4,10 @@ else
   set_toolchains("clang")
 end
 
-add_requires("bdwgc", {  system = false })
-
 set_warnings("allextra")
 
 target("plume-vm")
   add_rules("mode.release")
-  add_packages("bdwgc")
   add_files("src/**.c")
   add_includedirs("include")
   set_kind("binary") 
@@ -19,7 +16,6 @@ target("plume-vm")
 
 target("plume-vm-test")
   add_rules("mode.debug", "mode.profile")
-  add_packages("bdwgc")
   add_files("src/**.c")
   add_includedirs("include")
   set_targetdir("bin")
